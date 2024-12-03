ca059b58977e6ff8adf0ed9312dbd0afa55e918e
Tomer Kedem <tompha1@hotmail.com>
2024-12-03 12:54:27 +0200

    Link Shortcuts & Programmatic Navigation

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { TasksService } from '../tasks.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,RouterLink],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -16,15 +17,23 @@ export class NewTaskComponent {
   enteredSummary = signal('');
   enteredDate = signal('');
   private tasksService = inject(TasksService);
+  private router = inject(Router);
 
   onSubmit() {
+    console.log('this.userId()', this.userId());
     this.tasksService.addTask(
       {
         title: this.enteredTitle(),
         summary: this.enteredSummary(),
         date: this.enteredDate(),
       },
+     
+      
       this.userId()
     );
+
+    this.router.navigate(['/users', this.userId(),'tasks'], {
+      replaceUrl: true,
+    });
   }
 }
