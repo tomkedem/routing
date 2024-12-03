217c7a2c796f3b8142f7884498ab8ea2dcbd8509
Tomer Kedem <tompha1@hotmail.com>
2024-12-03 12:30:00 +0200

    Accessing Parent Route Data From Inside Nested Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
-
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
     {
@@ -16,7 +15,7 @@ export const routes: Routes = [
         children:[
             {
                 path: 'tasks', // <your-domain>/users/<uid>/tasks
-                component: TaskComponent
+                component: TasksComponent
             },
             {
                 path: 'tasks/new', // <your-domain>/users/<uid>/tasks/new
